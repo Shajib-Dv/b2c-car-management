b9b3baf864506aa4e7332afc64823997144e6c84
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-08-21 20:02:20 +0600

    basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-orange-700">
-      Hello world!
+      <h1 className="text-3xl font-bold text-orange-700">
+      Test
     </h1>
     </div>
   );
