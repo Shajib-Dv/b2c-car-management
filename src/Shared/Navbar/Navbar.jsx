6e2e0bd8f2299ee11f257c9998873d3c0df837ff
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-08-22 21:28:13 +0600

    nav and route

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <h1>nav</h1>
-            <Link to="/more">More</Link>
+            <div className='flex gap-5'>
+                <Link to="/newCar">NEW CAR</Link>
+                <Link to="/usedCar">USED CAR</Link>
+                <Link to="/sellCar">SELL CAR</Link>
+                <Link to="/compareCar">COMPARE CAR</Link>
+                <Link to="/newsAndReview">NEWS AND CAR</Link>
+                <Link to="/more">MORE CAR</Link>
+            </div>
         </div>
     );
 };
