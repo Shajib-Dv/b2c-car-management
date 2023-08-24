c052d050c8da9f461b768292b030020a35b58cf6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-08-24 10:37:35 +0600

    feel the best sections

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -3,12 +3,14 @@ import Navbar from '../Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
 import Cover from './Sections/Cover';
+import FeelNBrowseByBody from './Sections/FeelNBrowseByBody';
 
 const HomeLayouts = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Cover></Cover>
+            <FeelNBrowseByBody></FeelNBrowseByBody>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/Layouts/Sections/FeelNBrowseByBody.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BsDot } from 'react-icons/bs';
+
+const FeelNBrowseByBody = () => {
+    return (
+        <div className='bg-'>
+            <div className='flex items-center justify-between lg:pr-36'>
+                <div><img src="https://cdn.discordapp.com/attachments/1094651413235253289/1144124572556349490/Land-Rover-New-Range-Rover-Evoque-Exterior-170043_2.png" alt="" /></div>
+                <div className='flex flex-col gap-5'>
+                    <h1 className='text-2xl font-bold'>Feel the best experience <br /> with us</h1>
+                    <p className='text-[#4E4E4E] text-sm flex'><span className='text-base'><BsDot></BsDot></span> November 2021 (36,544 miles): Battery and driver-side <br /> high-pressure fuel pump replaced. An inspection was <br /> performed at this time as well.
+                    </p>
+                    <p className='text-[#4E4E4E] text-sm flex'><span className='text-base'><BsDot></BsDot></span> November 2021 (36,544 miles): Battery and driver-side <br /> high-pressure fuel pump replaced. An inspection was <br /> performed at this time as well.
+                    </p>
+                    <p className='text-[#4E4E4E] text-sm flex'><span className='text-base'><BsDot></BsDot></span> November 2021 (36,544 miles): Battery and driver-side <br /> high-pressure fuel pump replaced. An inspection was <br /> performed at this time as well.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeelNBrowseByBody;
