fbf11b0e74dd148fb83e590769d937027dd1dafd
Shajib-Dv <mdsojeeb242@gmail.com>
2023-08-26 13:23:53 +0600

    slider added

--- src/Page/ErrorPage/ErrorPage.jsx
+++ src/Page/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>err</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>err</h1>
+    </div>
+  );
 };
 
 export default ErrorPage;
---
+++ src/Shared/SectionSliders/BodyStyleSlider/BodyStyleSlider.jsx
@@ -0,0 +1,93 @@
+/** @format */
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
+const carsInfo = [
+  {
+    id: 1,
+    img: "https://cdn.discordapp.com/attachments/1094651413235253289/1144134548549013534/image_13.png",
+    name: "SUV",
+    price: 12627,
+  },
+  {
+    id: 2,
+    img: "https://cdn.discordapp.com/attachments/1094651413235253289/1144134548783906986/image_14.png",
+    name: "Seden",
+    price: 12627,
+  },
+  {
+    id: 3,
+    img: "https://cdn.discordapp.com/attachments/1094651413235253289/1144134549035569202/image_15.png",
+    name: "Pickup",
+    price: 12627,
+  },
+  {
+    id: 4,
+    img: "https://cdn.discordapp.com/attachments/1094651413235253289/1144134549580812388/image_16.png",
+    name: "Hatchback",
+    price: 12627,
+  },
+  {
+    id: 5,
+    img: "https://cdn.discordapp.com/attachments/1094651413235253289/1144134719127162990/image_17.png",
+    name: "Miniven",
+    price: 12627,
+  },
+];
+
+const BodyStyleSlider = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        loop={true}
+        autoplay={true}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
+      >
+        {carsInfo.map((car) => (
+          <SwiperSlide key={car.id}>
+            <div>
+              <img src={car.img} alt="" />
+              <div>
+                <h1 className="font-bold">{car.name}</h1>
+                <p className="text-xs">{car.price}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default BodyStyleSlider;
