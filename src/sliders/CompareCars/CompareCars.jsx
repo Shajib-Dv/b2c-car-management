66114803257f9abd9f7ac4a01bb3d0c7531875e3
Shajib-Dv <mdsojeeb242@gmail.com>
2023-08-28 17:24:09 +0600

    slider updated

--- src/sliders/CompareCars/CompareCars.jsx
+++ src/sliders/CompareCars/CompareCars.jsx
@@ -2,12 +2,13 @@
 
 import { BsFillArrowRightSquareFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import CompareCarsSlider from "./CompareCarsSlider";
 
 const CompareCars = () => {
   return (
     <>
-      <div className="flex gap-7 items-center px-10 mb-8">
-        <h1 className="text-2xl font-bold">Compare to buy the right car</h1>
+      <div className="flex md:gap-7 justify-between md:justify-start items-center md:px-10 px-4 mb-8">
+        <h1 className="md:text-2xl font-bold">Compare to buy the right car</h1>
         <Link>
           <div className="flex gap-2">
             <h1 className="text-sm mt-1 font-bold text-[#00A541]">View All</h1>
@@ -17,6 +18,7 @@ const CompareCars = () => {
           </div>
         </Link>
       </div>
+      <CompareCarsSlider />
     </>
   );
 };
--- src/sliders/CompareCars/CompareCarsSlider.jsx
+++ src/sliders/CompareCars/CompareCarsSlider.jsx
@@ -1,7 +1,135 @@
 /** @format */
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
 
+const carsInfo = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Toyota Land Cruiser",
+    name2: "Toyota Land Cruiser",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 12,
+    price2: 32,
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Hyundai",
+    name2: "Hyundai Nxt",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 127,
+    price2: 327,
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Virtus",
+    name2: "Volkswagen Virtus",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 17,
+    price2: 37,
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Toyota Cruiser",
+    name2: "Toyota Land",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 12,
+    price2: 32,
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Volkswagen",
+    name2: "Volkswagen Virtus",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 17,
+    price2: 37,
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/rvQbGPW/dubblecar.png",
+    name1: "Nexo",
+    name2: "Hyundai Nexo",
+    model1: "MTV1",
+    model2: "MTV2",
+    price1: 127,
+    price2: 327,
+  },
+];
 const CompareCarsSlider = () => {
-  return <div></div>;
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={0}
+        loop={true}
+        autoplay={true}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          390: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+        modules={[Navigation, Autoplay]}
+        className="mySwiper h-fit"
+      >
+        {carsInfo.map((car) => (
+          <SwiperSlide
+            key={car.id}
+            className="md:ml-2 bg-base-100 border rounded-md p-4"
+          >
+            <img src={car.img} alt="car" className="w-full md:h-64 bg-cover" />
+            <div className="text-black flex items-center justify-between mt-2">
+              <div className="flex-1">
+                <p>{car.model1}</p>
+                <h1 className="font-bold text-xl">{car.name1}</h1>
+                <p className="text-xl font-light">
+                  Rs {car.price1} - {Math.round(Math.random() * 500)} Lakh*
+                </p>
+              </div>
+              <div className="flex-1">
+                <p>{car.model2}</p>
+                <h1 className="font-bold text-xl">{car.name2}</h1>
+                <p className="text-xl font-light">
+                  Rs {car.price2} - {Math.round(Math.random() * 500)} Lakh*
+                </p>
+              </div>
+            </div>
+            <div className="mt-4 flex items-center justify-center w-full">
+              <Link to="/" className="btn-details">
+                Kia Carens vs Maruti XL6
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 };
 
 export default CompareCarsSlider;
