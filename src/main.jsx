581b75a07f837e8b8d27f4b4cf8649434d0c0694
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-08-29 12:02:17 +0600

    firebase auth setup

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import AuthProvider from './Provider/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-2xl mx-auto'>
-      <RouterProvider router={router} />
-    </div>
-    
+    <AuthProvider>
+      <div className='max-w-screen-2xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
+
   </React.StrictMode>,
 )
