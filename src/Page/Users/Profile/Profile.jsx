ea68f75f3aa036694e00af6bd7bb18b390c1108b
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-02 11:09:55 +0600

    userprofile route

--- src/Page/Users/Profile/Profile.jsx
+++ src/Page/Users/Profile/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Navbar from '../../../Shared/Navbar/Navbar';
+import Footer from '../../../Shared/Footer/Footer';
 
 const Profile = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1>Profile</h1>
+            <Footer></Footer>
         </div>
     );
 };
