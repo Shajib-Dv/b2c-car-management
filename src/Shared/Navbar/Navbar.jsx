41c5cbe81e90363675619efb240a37e2e0a388ea
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-02 13:15:11 +0600

    search bar issue

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
               <input
-                type="search"
+                
                 id="default-search"
                 className=" block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50   "
                 placeholder="Search Cars or Brands eg. Swift, or Maruti."
