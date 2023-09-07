edcc92e534cf453fa2355ab982a037db79b5971f
Shajib-Dv <mdsojeeb242@gmail.com>
2023-09-07 16:00:19 +0600

    car deatils page updated

--- src/Page/CarDetails/sections/EmiBanner.jsx
+++ src/Page/CarDetails/sections/EmiBanner.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+/** @format */
 
 const EmiBanner = () => {
-    return (
-        <div>
-            <h1>emi</h1>
-        </div>
-    );
+  return (
+    <div className="rounded-md shadow-md border-x-2 p-4 space-y-3 w-full">
+      <h1 className="title text-gray-400">EMI Starts</h1>
+      <p className="text-2xl font-bold">₹ 15,317 /Month </p>
+      <p>Interest calculated at 9.8% for 60 months</p>
+      <button className="btn-act">View Finance Offers</button>
+      <div>
+        <h2 className="title">Maruti Baleno Video</h2>
+        <iframe
+          className="rounded-lg"
+          width="100%"
+          height="350"
+          src="https://www.youtube.com/embed/Jl9D9PH8aFI?si=o9TIj3JR8qmDza6C"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default EmiBanner;
