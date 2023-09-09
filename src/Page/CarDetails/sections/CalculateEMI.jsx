3eebac223c8ea47259c19e04eed1702f30bd965c
Shajib-Dv <mdsojeeb242@gmail.com>
2023-09-09 13:21:34 +0600

    calculate emi res done

--- src/Page/CarDetails/sections/CalculateEMI.jsx
+++ src/Page/CarDetails/sections/CalculateEMI.jsx
@@ -2,9 +2,9 @@
 
 const CalculateEMI = () => {
   return (
-    <div className="mt-5 border rounded-md p-10">
+    <div className="my-5 border rounded-md p-10">
       <h2 className="title">Calculate your EMI</h2>
-      <div className="center-itm gap-5 justify-between my-10">
+      <div className="center-itm gap-5 flex-wrap md:flex-nowrap my-10">
         <select className="select-btn">
           <option defaultValue={"Select Brand"}>Select Brand</option>
           <option>Homer</option>
@@ -55,6 +55,90 @@ const CalculateEMI = () => {
           <p>0</p>
           <p>₹ 87,000</p>
         </div>
+      </div>
+      <div className="mt-10">
+        <p className="text-2xl text-gray-400 font-bold">
+          Loan Period {"( Years )"}
+        </p>
+
+        <div className="join md:w-1/2 my-5">
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="1"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="2"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="3"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="4"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="5"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="6"
+          />
+          <input
+            className="btn-cmn flex-1 join-item px-4"
+            type="radio"
+            name="options"
+            aria-label="7"
+          />
+        </div>
+
+        <div className="line-title">
+          <p>Total Loan Amount</p>
+          <p>₹ 9,73,698</p>
+        </div>
+        <hr />
+        <div className="line-title">
+          <p>Payable Amount</p>
+          <p>₹ 9,73,698</p>
+        </div>
+        <hr />
+        <div className="line-title">
+          <p>You'll pay extra</p>
+          <p>₹ 9,73,698</p>
+        </div>
+        <hr />
+
+        <div className="center-itm md:justify-evenly flex-col md:flex-row gap-20 md:gap-0 my-10">
+          <div>
+            <h2 className="text-5xl pb-2 font-bold">EMI</h2>
+            <p>Per Month</p>
+          </div>
+          <div>
+            <h2 className="text-5xl pb-2 font-bold">₹20,593</h2>
+            <p>Calculated on On Road Price</p>
+          </div>
+        </div>
+        <hr />
+
+        <p className="text-center mt-10 font-bold">
+          At CarDekho, we can help you get the best deal on your loans. Please
+          call us on <span className="text-green-600">1800 200 3000</span> for
+          help.
+        </p>
       </div>
     </div>
   );
