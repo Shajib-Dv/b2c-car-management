1de2fa9e1b6342ff3ee9532750bd91b183427036
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-11 11:56:34 +0600

    used car sections reedit and place

--- src/Page/UsedCar/UsedCar.jsx
+++ src/Page/UsedCar/UsedCar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import UsedCarCover from './Sections/UsedCarCover';
+import BrowseByMake from './Sections/BrowseByMake';
+import BrowseByBody from './Sections/BrowseByBody';
+import Ads from '../../Layouts/Sections/Ads';
+import Reviews from '../../Layouts/Sections/Reviews';
+import FooterCoaver from '../../Layouts/Sections/FooterCoaver';
 
 const UsedCar = () => {
     return (
         <div>
-            <h1>used cars</h1>
+            <div className='mb-20'>
+                <UsedCarCover />
+            </div>
+            <BrowseByMake />
+            <BrowseByBody />
+            <Ads />
+            <div className='mt-10'>
+                <Reviews />
+            </div>
+            <FooterCoaver />
         </div>
     );
 };
