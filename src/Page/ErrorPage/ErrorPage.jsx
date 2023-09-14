4a5a4b3f94f0ee7c91a3f4d8b54bcc9b111825a0
Shajib-Dv <mdsojeeb242@gmail.com>
2023-09-14 22:09:40 +0600

    error page updated

--- src/Page/ErrorPage/ErrorPage.jsx
+++ src/Page/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,17 @@
 /** @format */
 
-import React from "react";
-
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>err</h1>
+    <div className="min-h-screen flex items-center justify-center flex-col bg-black">
+      <div className="relative w-full md:w-3/4 center-itm justify-center">
+        <img
+          src="https://media.discordapp.net/attachments/1149991717647421440/1151899359231868969/image0.png"
+          alt="Salarput"
+        />
+        <div className="absolute bottom-0 md:w-1/2 text-center bg-pink-500 rounded-md p-2 text-white">
+          ও রে চোদানির ফোয়া, এতো চেষ্টার পরেও আর লুকায়ি থাকতে ফারলাম না 😠
+        </div>
+      </div>
     </div>
   );
 };
