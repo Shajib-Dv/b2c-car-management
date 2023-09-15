c563f6b17a296d6a4efcb8efa2cb3d33ca916c94
Shajib-Dv <mdsojeeb242@gmail.com>
2023-09-15 15:29:57 +0600

    404 updated

--- src/Page/ErrorPage/ErrorPage.jsx
+++ src/Page/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,36 @@
 /** @format */
 
+import { useEffect } from "react";
+import { useState } from "react";
+
 const ErrorPage = () => {
+  const [exit, setExit] = useState(null);
+
+  const [stop, setStop] = useState(false);
+
+  useEffect(() => {
+    if (!stop) {
+      exit?.target?.classList.add("animate-bounce");
+    } else {
+      exit?.target?.classList.remove("animate-bounce");
+    }
+  }, [exit, stop]);
+
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col bg-black">
-      <div className="relative w-full md:w-3/4 center-itm justify-center">
+    <div
+      onClick={() => setStop(!stop)}
+      className="min-h-screen flex items-center justify-center flex-col bg-black "
+    >
+      <div
+        onMouseEnter={(e) => setExit(e)}
+        className="relative w-full md:w-3/4 center-itm justify-center"
+      >
         <img
           src="https://media.discordapp.net/attachments/1149991717647421440/1151899359231868969/image0.png"
           alt="Salarput"
         />
         <div className="absolute bottom-0 md:w-1/2 text-center bg-pink-500 rounded-md p-2 text-white">
-          ও রে চোদানির ফোয়া, এতো চেষ্টার পরেও আর লুকায়ি থাকতে ফারলাম না 😠
+          এতো চেষ্টার পরেও আর লুকায়ি থাকতে ফারলাম না, চোদানির ফোয়া
         </div>
       </div>
     </div>
