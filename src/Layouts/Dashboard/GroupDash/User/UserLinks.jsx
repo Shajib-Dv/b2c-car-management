785dc2995ccacdc5f36a925ec0dee6fc73e5b400
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-27 18:52:46 +0600

    user link added

--- src/Layouts/Dashboard/GroupDash/User/UserLinks.jsx
+++ src/Layouts/Dashboard/GroupDash/User/UserLinks.jsx
@@ -2,6 +2,12 @@
 
 import { MdOutlineCarRepair, MdOutlineCarRental } from "react-icons/md";
 import ActiveDashLink from "../../ActiveDashLink";
+import { BiDetail } from 'react-icons/bi';
+import { FaAddressBook } from 'react-icons/fa';
+import { HiOutlineBanknotes } from 'react-icons/hi2';
+import { BsBookmarkHeart } from 'react-icons/bs';
+import { AiOutlineTransaction } from 'react-icons/ai';
+import { IoGitCompare } from 'react-icons/io5';
 
 const UserLinks = () => {
   return (
@@ -11,6 +17,24 @@ const UserLinks = () => {
       </ActiveDashLink>
       <ActiveDashLink to="/dashboard/user/order_summary">
         <MdOutlineCarRental className="text-2xl" /> Order Summary
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/MY_Bids_List">
+        <BiDetail className="text-2xl" />My Bids List
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/addresses">
+        <FaAddressBook className="text-2xl" /> Addresses
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/EMI_Calculator">
+        <HiOutlineBanknotes className="text-2xl" />EMI Calculator
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/Wish_List">
+        <BsBookmarkHeart className="text-2xl" />Wish List
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/Transaction">
+        <AiOutlineTransaction className="text-2xl" />Your Transactions
+      </ActiveDashLink>
+      <ActiveDashLink to="/dashboard/user/Compare_Car">
+        <IoGitCompare className="text-2xl" />Compare Car
       </ActiveDashLink>
     </>
   );
