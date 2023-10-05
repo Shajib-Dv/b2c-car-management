2b2e34d4ad073619e9390643f528199b51dfd6e6
Shajib-Dv <mdsojeeb242@gmail.com>
2023-10-05 14:06:43 +0600

    recent car loaded

---
+++ src/Shared/components/Loader.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+const Loader = () => {
+  return (
+    <div className="min-h-full w-full center-itm justify-center bg-transparent">
+      <span className="loading text-green-600"></span>
+    </div>
+  );
+};
+
+export default Loader;
