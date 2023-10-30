2af0e97b01e50993884a44d1baf70c120d463a95
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-10-30 12:42:10 +0600

    order view as modal

--- src/Layouts/Dashboard/GroupDash/User/Sections/OrderSummary.jsx
+++ src/Layouts/Dashboard/GroupDash/User/Sections/OrderSummary.jsx
@@ -1,8 +1,9 @@
-import { GiConfirmed } from "react-icons/gi";
+import { GiConfirmed, GiPresent } from "react-icons/gi";
 import { FaClipboardList } from "react-icons/fa";
 import getOrderList from "../../../../../utils/getOrderList";
 import { Link } from "react-router-dom";
-import { MdPendingActions } from "react-icons/md";
+import { MdCancelPresentation, MdPendingActions } from "react-icons/md";
+import { RxCrossCircled } from "react-icons/rx";
 import UserOrderSummary from "../../../Modal/UserOrderSummary";
 import { useState } from "react";
 import EmptyData from "../../../../../Shared/components/EmptyData";
@@ -38,31 +39,52 @@ const OrderSummary = () => {
               <div className="hover:shadow-lg" key={index}>
                 <div className="flex justify-between bg-slate-100 border-[1px] p-4 items-center rounded-t-md">
                   <div>
-                    <h1 className="font-bold">Order# {order.orderInfo.orderId}</h1>
-                    <h1 className="text-sm">Date Added: {order.orderInfo.date}</h1>
+                    <h1 className="font-bold">Order# {order?.orderInfo?.orderId}</h1>
+                    <h1 className="text-sm">Date Added: {order?.orderInfo?.date}</h1>
                   </div>
                   <div>
                     {
-                      order.orderInfo.status === "processing" ? (
+                      order?.orderInfo?.status === "processing" ? (
                         <h1 className="flex items-center gap-1">
                           <span className="text-green-600 text-lg">
                             <MdPendingActions />
                           </span>
                           Processing
                         </h1>
-                      ) : order.orderInfo.status === "error" ? (
+                      ) : order?.orderInfo?.status === "something_wrong" ? (
                         <h1 className="flex items-center gap-1">
-                          <span className="text-green-600 text-lg">
-                            <MdPendingActions />
+                          <span className="text-red-500 text-lg">
+                            <RxCrossCircled />
                           </span>
                           Contact <span className="text-red-500 font-bold"><Link>support</Link></span> or submit a <span className="text-red-500 font-bold"><Link>ticket</Link></span>
                         </h1>
-                      ) : (
+                      ) : order?.orderInfo?.status === "canceled" ? (
+                        <h1 className="flex items-center gap-1">
+                          <span className="text-yellow-400 text-lg">
+                            <MdCancelPresentation />
+                          </span>
+                          Canceled Order Again
+                        </h1>
+                      ) : order?.orderInfo?.status === "confirmed" ? (
                         <h1 className="flex items-center gap-1">
                           <span className="text-green-600 text-lg">
                             <GiConfirmed />
                           </span>
-                          Completed
+                          Your Order has been Confirmed
+                        </h1>
+                      ) : order?.orderInfo?.status === "sent" ? (
+                        <h1 className="flex items-center gap-1">
+                          <span className="text-green-600 text-lg">
+                            <GiPresent />
+                          </span>
+                          Your Order On the Way
+                        </h1>
+                      ) : (
+                        <h1 className="flex items-center gap-1">
+                          <span className="text-cyan-400 text-lg">
+                            <MdPendingActions />
+                          </span>
+                          Processing
                         </h1>
                       )
                     }
@@ -70,9 +92,9 @@ const OrderSummary = () => {
 
                 </div>
                 <div className="bg-[#FAFAFA] p-4 border-[1px] flex justify-between items-center rounded-b-md">
-                  <h1 className="font-semibold text-green-900">
-                    Total Ordered Items: {order.order.length} <br />
-                    Total Price: {order.orderInfo.totalPrice} RS.
+                  <h1 className="font-semibold text-green-600">
+                    Total Ordered Items: {order?.order?.length} <br />
+                    Total Price: {order?.orderInfo?.totalPrice} RS.
                   </h1>
                   <button onClick={() => handleOpenModal(order)} className="btn btn-success text-white">View</button>
                 </div>
